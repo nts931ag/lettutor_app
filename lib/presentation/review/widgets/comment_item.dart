10ada5af42b9254ef6e1beb9400e684da91ecd36
Nguyen Thai Son <95063137+nts931ag@users.noreply.github.com>
2023-03-14 14:07:31 +0700

    implement new style of comment item.

--- lib/presentation/review/widgets/comment_item.dart
+++ lib/presentation/review/widgets/comment_item.dart
@@ -2,10 +2,9 @@ import 'package:flutter/material.dart';
 import 'package:flutter_rating_bar/flutter_rating_bar.dart';
 import 'package:get/get.dart';
 import 'package:lettutor_app/presentation/common/widgets/icon/circle_box_widget.dart';
+import 'package:lettutor_app/res/colors/colors_core.dart';
 import 'package:lettutor_app/res/dimens.dart';
 import 'package:lettutor_app/res/theme/text_theme.dart';
-
-
 
 class CommentItem extends StatelessWidget {
   Widget image;
@@ -13,6 +12,7 @@ class CommentItem extends StatelessWidget {
   String time;
   double rating;
   String message;
+
   CommentItem({
     Key? key,
     required this.image,
@@ -24,55 +24,150 @@ class CommentItem extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Row(
-      crossAxisAlignment: CrossAxisAlignment.start,
-      children: [
-        CircleBox(size: 40.w, child: image),
-        SizedBox(width: 10.w),
-        Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Row(
-              children: [
-                Text(
-                  name,
-                  style: text15.copyWith(fontWeight: FontWeight.w600),
-                ),
-                SizedBox(width: 5.w),
-                Text(
-                  time,
-                  style: text13.copyWith(color: Colors.grey),
-                )
-              ],
-            ),
-            RatingBar.builder(
-              initialRating: rating,
-              minRating: rating,
-              maxRating: rating,
-              direction: Axis.horizontal,
-              allowHalfRating: true,
-              itemCount: 5,
-              itemSize: 15,
-              itemPadding: const EdgeInsets.symmetric(horizontal: 3.0),
-              itemBuilder: (context, _) => const Icon(
-                Icons.star,
-                color: Colors.amber,
-                size: 5,
+    // return Row(
+    //   crossAxisAlignment: CrossAxisAlignment.start,
+    //   children: [
+    //     CircleBox(size: 40.w, child: image),
+    //     SizedBox(width: 10.w),
+    //     Column(
+    //       crossAxisAlignment: CrossAxisAlignment.start,
+    //       children: [
+    //         Row(
+    //           children: [
+    //             Text(
+    //               name,
+    //               style: text15.copyWith(fontWeight: FontWeight.w600),
+    //             ),
+    //             SizedBox(width: 5.w),
+    //             Text(
+    //               time,
+    //               style: text13.copyWith(color: Colors.grey),
+    //             )
+    //           ],
+    //         ),
+    //         RatingBar.builder(
+    //           initialRating: rating,
+    //           minRating: rating,
+    //           maxRating: rating,
+    //           direction: Axis.horizontal,
+    //           allowHalfRating: true,
+    //           itemCount: 5,
+    //           itemSize: 15,
+    //           itemPadding: const EdgeInsets.symmetric(horizontal: 3.0),
+    //           itemBuilder: (context, _) => const Icon(
+    //             Icons.star,
+    //             color: Colors.amber,
+    //             size: 5,
+    //           ),
+    //           onRatingUpdate: (rating) {},
+    //         ),
+    //         SizedBox(
+    //           width: Get.width - 100.w,
+    //           child: Text(
+    //             message,
+    //             style: text13,
+    //             overflow: TextOverflow.ellipsis,
+    //             maxLines: 4,
+    //           ),
+    //         ),
+    //       ],
+    //     )
+    //   ],
+    // );
+    return _buildNewCommentItemStyle();
+  }
+
+  Widget _buildNewCommentItemStyle() {
+    return Container(
+      margin: EdgeInsets.symmetric(horizontal: 5.h),
+      padding: EdgeInsets.symmetric(horizontal: 15.h, vertical: 10.w),
+      decoration: BoxDecoration(
+          color: Colors.white,
+          borderRadius: BorderRadius.circular(10.r),
+          boxShadow: [
+            BoxShadow(
+                color: Colors.grey.withOpacity(0.8),
+                spreadRadius: 2.r,
+                blurRadius: 3.r,
+                offset: Offset(0, 3))
+          ]),
+      child: Column(
+        children: <Widget>[
+          Container(
+              child: Row(
+            children: <Widget>[
+              CircleBox(size: 40.w, child: image),
+              SizedBox(
+                width: 10.w,
               ),
-              onRatingUpdate: (rating) {},
-            ),
-            SizedBox(
-              width: Get.width - 100.w,
-              child: Text(
-                message,
-                style: text13,
-                overflow: TextOverflow.ellipsis,
-                maxLines: 4,
+              Column(
+                mainAxisAlignment: MainAxisAlignment.spaceAround,
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: <Widget>[
+                  Row(
+                    children: [
+                      Text(
+                        name,
+                        style: text15.copyWith(fontWeight: FontWeight.w600),
+                      ),
+                      SizedBox(
+                        width: 10.w,
+                      ),
+                      Text(
+                        time,
+                        style: text13.copyWith(color: Colors.grey),
+                      ),
+                    ],
+                  ),
+                  Row(
+                    children: [
+                      Text(
+                        '${rating}',
+                        style: TextStyle(color: Colors.amber),
+                      ),
+                      SizedBox(
+                        width: 5.w,
+                      ),
+                      Container(
+                        width: 100.w,
+                        child: RatingBar.builder(
+                          initialRating: rating,
+                          minRating: rating,
+                          maxRating: rating,
+                          direction: Axis.horizontal,
+                          allowHalfRating: true,
+                          itemCount: 5,
+                          itemSize: 15,
+                          itemPadding:
+                              const EdgeInsets.symmetric(horizontal: 3.0),
+                          itemBuilder: (context, _) => const Icon(
+                            Icons.star,
+                            color: Colors.amber,
+                            size: 5,
+                          ),
+                          onRatingUpdate: (rating) {},
+                        ),
+                      ),
+                    ],
+                  )
+                ],
               ),
+            ],
+          )),
+          SizedBox(
+            height: 5.h,
+          ),
+          SizedBox(
+            width: Get.width - 100.w,
+            child: Text(
+              message,
+              style: text13,
+              overflow: TextOverflow.ellipsis,
+              maxLines: 4,
             ),
-          ],
-        )
-      ],
+          )
+        ],
+      ),
     );
   }
 }
